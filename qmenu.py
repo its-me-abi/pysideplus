ef1cfe394098e57461252bc106863f0815eeae1f
abi <abysemail@zohomail.in>
2025-02-15 17:43:55 +0530

    Add files via upload

--- qmenu.py
+++ qmenu.py
@@ -1,8 +1,11 @@
-from PySide6.QtWidgets import  QMenu
+from PySide6.QtWidgets import  QMenu,QScrollArea,QWidget,QVBoxLayout
 from PySide6 import QtGui
 from PySide6 import QtCore
 import styleconfig
 
+"""
+this is pyside6 qmenu modified .round edges and light border
+"""
 
 class CustomQmenu(QMenu):
     """
@@ -14,9 +17,11 @@ class CustomQmenu(QMenu):
         self.radius = 10
         self.setStyleSheet(self.__get_qmenu_style())
 
+
     def __get_qmenu_style(self):
         return f"""
                     QMenu {{
+                        
                         background-color: #ffffff;
                         border: 2px solid {styleconfig.Qmenu.border_color};
                         border-radius: {self.radius}px; 
@@ -49,8 +54,8 @@ if __name__ == "__main__":
 
     app = QApplication(sys.argv)
     context_menu = CustomQmenu()
-    for x in range(1,10):
-        close_action = QAction(f"testing.. please select {x}",context_menu)
+    for x in range(1,30):
+        close_action = QAction(f"testing. select {x}",context_menu)
         close_action.triggered.connect(lambda y,selected=x:( print("it's working, selected" ,selected),sys.exit()))
         context_menu.addAction(close_action)
     context_menu.exec(QtCore.QPoint(300,300))
