e9c42e2b4f1b39ff91d3505ab976796b9160550e
abi <abysemail@zohomail.in>
2025-03-11 23:53:28 +0530

    latest

--- scrollable_qmenu.py
+++ scrollable_qmenu.py
@@ -2,12 +2,15 @@ from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, Q
 from PySide6.QtGui import QFont,QAction,QIcon
 from qmenu import CustomQmenu as QMenu
 from PySide6.QtCore import QPoint
+
 """
 ai generated code for pyside6 scrollable qmenu
 """
+
 class ScrollableMenu(QMenu):
     def __init__(self,*args,**kargs):
         super().__init__(*args,**kargs)
+        self.hover_color = "#f1f1f1"
         # Create Scroll Area
         self.scroll = QScrollArea()
         self.scroll.setWidgetResizable(True)
@@ -22,12 +25,26 @@ class ScrollableMenu(QMenu):
         self.setLayout(QVBoxLayout())  # Menu layout
         self.layout().addWidget(self.scroll)
 
-    def addAction(self, action):
+    def addAction(self, action,hover_color=None):
         """Override addAction to insert into the scrollable area"""
+        if hover_color == None:
+            hover_color = self.hover_color
         btn = QPushButton(action.text())
         btn.clicked.connect(action.triggered.emit)  # Link button to action
         btn.setFlat(True)  # Make it look like a menu item
-        btn.setStyleSheet("QPushButton { text-align: left;}")
+        btn.setObjectName("addbutton")
+        btn.setStyleSheet(f"""#addbutton {{
+                                 text-align: left;
+                                 border: 0px solid red;
+                                 padding: 5px;
+                                 background-color:white;
+                                 }}
+                            #addbutton:hover {{
+                                background-color: {hover_color};
+                                color : black;
+                                }}
+
+                            """)
         self.menu_layout.addWidget(btn)
 
     def showEvent(self, event):
